65b4b7edf2f4bf0f23ce83c776c833307105bb84
kemalelmizan <kemalelmizan@gmail.com>
2018-09-10 18:19:50 +0800

    change port to 5000

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         "127.0.0.1:1234",
+		Addr:         ":5000",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
